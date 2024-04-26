10397cd9b4db32a3cd47f40fe6a4701239d4d683
vb505 <82651157+vb505@users.noreply.github.com>
2024-04-26 10:31:05 +0200

    Update deliver.sh grep

--- jenkins/scripts/deliver.sh
+++ jenkins/scripts/deliver.sh
@@ -11,17 +11,16 @@ set +x
 echo 'The following command extracts the value of the <name/> element'
 echo 'within <project/> of your Java/Maven project''s "pom.xml" file.'
 set -x
-NAME=`mvn -q -DforceStdout help:evaluate -Dexpression=project.name`
+NAME=`mvn -q -DforceStdout help:evaluate -Dexpression=project.name` | grep -v "^\[" | sed 's/\x1b\[[0-9;]*m//g')
 set +x
 
 echo 'The following command behaves similarly to the previous one but'
 echo 'extracts the value of the <version/> element within <project/> instead.'
 set -x
-VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`
+VERSION=`mvn -q -DforceStdout help:evaluate -Dexpression=project.version`| grep -E "^[0-9]+\.[0-9]+.*"`
 set +x
 
 echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 set -x
-echo java -jar target/${NAME}-${VERSION}.jar
-java -jar target/my-app-1.0-SNAPSHOT.jar
+java -jar target/${NAME}-${VERSION}.jar
